bf42b55670df204ec77199cac7ee2e3ed0514dd6
Mike Boan <mikeboan@gmail.com>
2017-10-09 12:24:48 -0400

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-questions = Question.create([
+Question.destroy_all
+questions = Question.create!([
   { body: "Why are you interested in the position?"} ,
   { body: "Why would you be a good fit for the position?" },
   { body: "What would you pay attention to if you were deciding whom to admit into App Academy?" },
   { body: "Please describe your relevant teaching experience (if any)." }
 ])
 
-apps = Application.create([
+App.destroy_all
+apps = App.create!([
   { first_name: "Mike", last_name: "Boan", email: "mboan@appacademy.io" },
   { first_name: "Maurice", last_name: "Roach", email: "mroach@appacademy.io" }
 ])
 
-responses = Response.create([
-  { question_id: questions[0], application_id: apps[0], body: "For the money" },
-  { question_id: questions[1], application_id: apps[0], body: "I need money" },
-  { question_id: questions[2], application_id: apps[0], body: "If they're smart" },
-  { question_id: questions[3], application_id: apps[0], body: "None" },
-  { question_id: questions[0], application_id: apps[1], body: "It's cool" },
-  { question_id: questions[1], application_id: apps[1], body: "I can do it" },
-  { question_id: questions[2], application_id: apps[1], body: "If they can do it" },
-  { question_id: questions[3], application_id: apps[1], body: "Stuff" },
+Response.destroy_all
+responses = Response.create!([
+  { question_id: questions[0].id, app_id: apps[0].id, body: "For the money" },
+  { question_id: questions[1].id, app_id: apps[0].id, body: "I need money" },
+  { question_id: questions[2].id, app_id: apps[0].id, body: "If they're smart" },
+  { question_id: questions[3].id, app_id: apps[0].id, body: "None" },
+  { question_id: questions[0].id, app_id: apps[1].id, body: "It's cool" },
+  { question_id: questions[1].id, app_id: apps[1].id, body: "I can do it" },
+  { question_id: questions[2].id, app_id: apps[1].id, body: "If they can do it" },
+  { question_id: questions[3].id, app_id: apps[1].id, body: "Stuff" },
 ])
