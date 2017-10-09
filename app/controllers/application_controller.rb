5f2caf3da95be0798b7b6d0f294eceb44d226cf2
Mike Boan <mikeboan@gmail.com>
2017-10-09 12:09:30 -0400

    Add auth stuff to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_admin, :current_user_name, :current_user_id
+
+  def current_admin
+    @current_admin ||= Admin.find_by(session_token: session[:session_token])
+  end
+
+  def login!(admin)
+    @current_admin = admin
+    session[:session_token] = admin.reset_session_token!
+  end
+
+  def current_admin_id
+    current_admin ? current_admin.id : nil
+  end
+
+  def require_current_admin!
+    redirect_to new_admin_url unless current_admin
+  end
+
+  def require_no_current_admin!
+    redirect_to root_url if current_admin
+  end
 end
